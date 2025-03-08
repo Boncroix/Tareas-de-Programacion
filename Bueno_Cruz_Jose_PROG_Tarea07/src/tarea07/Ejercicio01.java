90fc5a9a22e964aa0a0d4422f7b3b9e11223d55a
Boncroix <boncroix@gmail.com>
2025-03-08 07:36:29 +0100

    Ejercicio 1 terminado

--- Bueno_Cruz_Jose_PROG_Tarea07/src/tarea07/Ejercicio01.java
+++ Bueno_Cruz_Jose_PROG_Tarea07/src/tarea07/Ejercicio01.java
@@ -2,6 +2,8 @@ package tarea07;
 
 import java.util.Set;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 /**
  * Ejercicio 1. Creando conjuntos de ciclos DAW y DAM con sus módulos
@@ -39,25 +41,43 @@ public class Ejercicio01 {
         //----------------------------------------------
         
         // Inicializamos los arrays que vamos a utilizar
-
+        String[] arrayModulosDAM = Utilidades.getArrayModulosDAM();
+        String[] arrayModulosDAW = Utilidades.getArrayModulosDAW();
 
         
         // Inicialimando las colecciones que vamos autilizar
         // Al usar conjuntos (Set) garantizamos que no se pueden repetir elementos
+        Set<String> modulosDAM = new HashSet<>();
+        Set<String> modulosDAW = new HashSet<>();
         
         
         // Rellenamos los conjuntos de los ciclos con sus módulos correspondientes
+        for (String modulo : arrayModulosDAM) {
+            if (!modulosDAM.add(modulo)) {
+                System.out.printf("No se pudo añadir. El módulo %s ya está en la lista", modulo);
+            }
+        }
+        
+        for (String modulo : arrayModulosDAW) {
+            if (!modulosDAW.add(modulo)) {
+                System.out.printf("No se pudo añadir. El módulo %s ya está en la lista", modulo);
+            }
+        }
        
 
         // Unión de los dos conjuntos
         
+        Set<String> union = new HashSet<>(modulosDAM);
+        union.addAll(modulosDAW);
+        
 
         // Intersección de los conjuntos
-        
-
+        Set<String> interseccion = new HashSet<>(modulosDAM);
+        interseccion.retainAll(modulosDAW);
         
         // Diferencia de los conjuntos
-        
+        Set<String> diferencia = new HashSet<>(modulosDAM);
+        diferencia.removeAll(modulosDAW);
 
         
         //----------------------------------------------
@@ -67,18 +87,41 @@ public class Ejercicio01 {
         // Recorremos los conjuntos y mostramos su contenido por pantalla
         
         System.out.printf ("Conjunto C1 (módulos DAW):\n");
+        int index = 1;
+        for (String modulo : modulosDAW) {
+            System.out.printf("%d. %s\n", index, modulo);
+            index++;
+        }
 
 
         System.out.printf ("\nConjunto C2 (módulos DAM):\n");
+        index = 1;
+        for (String modulo : modulosDAM) {
+            System.out.printf("%d. %s\n", index, modulo);
+            index++;
+        }
 
         
         System.out.printf ("\nUnión C1 y C2:\n");
+        index = 1;
+        for (String modulo : union) {
+            System.out.printf("%d. %s\n", index, modulo);
+            index++;
+        }
 
         
         System.out.printf ("\nInterseccion C1 y C2:\n");
-
+        index = 1;
+        for (String modulo : interseccion) {
+            System.out.printf("%d. %s\n", index, modulo);
+            index++;
+        }
         
         System.out.printf ("\nDiferencia C1 y C2:\n");
-
+        index = 1;
+        for (String modulo : diferencia) {
+            System.out.printf("%d. %s\n", index, modulo);
+            index++;
+        }
     }
 }
