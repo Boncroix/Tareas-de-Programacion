f48545db81b66d938a8686217a08059f294b4dda
Boncroix <boncroix@gmail.com>
2025-03-09 10:03:03 +0100

    Ejerccicio 2 acabado

--- Bueno_Cruz_Jose_PROG_Tarea07/src/tarea07/Ejercicio01.java
+++ Bueno_Cruz_Jose_PROG_Tarea07/src/tarea07/Ejercicio01.java
@@ -1,37 +1,15 @@
 package tarea07;
 
-import java.util.Set;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Ejercicio 1. Creando conjuntos de ciclos DAW y DAM con sus módulos
- * @author 
+ *
+ * @author José Bueno Cruz
  */
 public class Ejercicio01 {
     
     public static void main(String[] args) {
-
-        //----------------------------------------------
-        //          Declaración de variables 
-        //----------------------------------------------
-        
-        // Constantes
-
-        // Variables de entrada
-
-        // Variables auxiliares
-
-                
-        // Variables de salida
-
-     
-        //----------------------------------------------
-        //                Entrada de datos 
-        //----------------------------------------------
-        
-        // No hay entrada de datos, pues se usa un número fijo de elementos
         
         System.out.println("CONJUNTOS DE MÓDULOS DE DAW Y DAM");
         System.out.println("--------------------------------");
@@ -39,89 +17,45 @@ public class Ejercicio01 {
         //----------------------------------------------
         //                  Procesamiento
         //----------------------------------------------
-        
         // Inicializamos los arrays que vamos a utilizar
         String[] arrayModulosDAM = Utilidades.getArrayModulosDAM();
         String[] arrayModulosDAW = Utilidades.getArrayModulosDAW();
 
-        
-        // Inicialimando las colecciones que vamos autilizar
+        // Inicialimando y rellenamos las colecciones que vamos autilizar
         // Al usar conjuntos (Set) garantizamos que no se pueden repetir elementos
-        Set<String> modulosDAM = new HashSet<>();
-        Set<String> modulosDAW = new HashSet<>();
-        
-        
-        // Rellenamos los conjuntos de los ciclos con sus módulos correspondientes
-        for (String modulo : arrayModulosDAM) {
-            if (!modulosDAM.add(modulo)) {
-                System.out.printf("No se pudo añadir. El módulo %s ya está en la lista", modulo);
-            }
-        }
-        
-        for (String modulo : arrayModulosDAW) {
-            if (!modulosDAW.add(modulo)) {
-                System.out.printf("No se pudo añadir. El módulo %s ya está en la lista", modulo);
-            }
-        }
-       
+        Set<String> modulosDAM = new HashSet<>(Arrays.asList(arrayModulosDAM));
+        Set<String> modulosDAW = new HashSet<>(Arrays.asList(arrayModulosDAW));
 
         // Unión de los dos conjuntos
-        
         Set<String> union = new HashSet<>(modulosDAM);
         union.addAll(modulosDAW);
-        
 
         // Intersección de los conjuntos
         Set<String> interseccion = new HashSet<>(modulosDAM);
         interseccion.retainAll(modulosDAW);
-        
+
         // Diferencia de los conjuntos
         Set<String> diferencia = new HashSet<>(modulosDAM);
         diferencia.removeAll(modulosDAW);
 
-        
         //----------------------------------------------
         //              Salida de Resultados 
         //----------------------------------------------
-        
         // Recorremos los conjuntos y mostramos su contenido por pantalla
+        System.out.printf("Conjunto C1 (módulos DAW):\n");
+        Utilidades.mostrarModulos(modulosDAW);
         
-        System.out.printf ("Conjunto C1 (módulos DAW):\n");
-        int index = 1;
-        for (String modulo : modulosDAW) {
-            System.out.printf("%d. %s\n", index, modulo);
-            index++;
-        }
-
-
-        System.out.printf ("\nConjunto C2 (módulos DAM):\n");
-        index = 1;
-        for (String modulo : modulosDAM) {
-            System.out.printf("%d. %s\n", index, modulo);
-            index++;
-        }
-
+        System.out.printf("\nConjunto C2 (módulos DAM):\n");
+        Utilidades.mostrarModulos(modulosDAM);
         
-        System.out.printf ("\nUnión C1 y C2:\n");
-        index = 1;
-        for (String modulo : union) {
-            System.out.printf("%d. %s\n", index, modulo);
-            index++;
-        }
-
+        System.out.printf("\nUnión C1 y C2:\n");
+        Utilidades.mostrarModulos(union);
         
-        System.out.printf ("\nInterseccion C1 y C2:\n");
-        index = 1;
-        for (String modulo : interseccion) {
-            System.out.printf("%d. %s\n", index, modulo);
-            index++;
-        }
+        System.out.printf("\nInterseccion C1 y C2:\n");
+        Utilidades.mostrarModulos(interseccion);
         
-        System.out.printf ("\nDiferencia C1 y C2:\n");
-        index = 1;
-        for (String modulo : diferencia) {
-            System.out.printf("%d. %s\n", index, modulo);
-            index++;
-        }
+        System.out.printf("\nDiferencia C1 y C2:\n");
+        Utilidades.mostrarModulos(diferencia);
     }
+
 }
--- Bueno_Cruz_Jose_PROG_Tarea07/src/tarea07/Ejercicio02.java
+++ Bueno_Cruz_Jose_PROG_Tarea07/src/tarea07/Ejercicio02.java
@@ -1,110 +1,120 @@
 package tarea07;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-/** Ejercicio 2. Trabajando con listas de módulos
- * @author   
+/**
+ * Ejercicio 2. Trabajando con listas de módulos
+ *
+ * @author José Bueno Cruz
  */
 public class Ejercicio02 {
 
     public static void main(String[] args) {
-        
-        //----------------------------------------------
-        //          Declaración de variables 
-        //----------------------------------------------
-        
-        // Constantes
-        
-        // Variables de entrada
-        
-        // Variables auxiliares
-        
-        
-        // Variables de salida
 
-
-        
-        //----------------------------------------------
-        //               Entrada de datos 
-        //----------------------------------------------
-        
-        // No hay entrada de datos, pues se usa un número fijo de elementos
-        
         System.out.println("LISTAS DE MÓDULOS");
         System.out.println("-----------------------------");
 
         // Inicializamos los arrays que vamos a utilizar
+        String[] arrayModulosDAM = Utilidades.getArrayModulosDAM();
+        String[] arrayModulosDAW = Utilidades.getArrayModulosDAW();
 
-
-        // Instanciamos las colecciones para trabajar con ellas
-
-        
-        // Rellenamos las listas de los ciclos con sus módulos correspondientes
-
-
+        // Instanciamos e inicializamos las colecciones para trabajar con ellas
+        List<String> modulosDAM = new ArrayList<>(Arrays.asList(arrayModulosDAM));
+        List<String> modulosDAW = new ArrayList<>(Arrays.asList(arrayModulosDAW));
 
         //----------------------------------------------
         //               Procesamiento
         //----------------------------------------------
-
         //Instanciamos las variables necesarias
-
+        List<String> modulosmatriculadosList = new ArrayList<>();
+        Set<String> modulosMatriculadosSet = new HashSet<>();
+        Iterator<String> iterDAW = modulosDAW.iterator();
 
         System.out.println();
         System.out.printf("Contenido inicial de la lista de módulos de DAW:  \n");
+        Utilidades.mostrarModulos(modulosDAW);
 
-        
-        System.out.println();   
+        System.out.println();
         System.out.printf("Contenido inicial de la lista de módulos de DAM:  \n");
+        Utilidades.mostrarModulos(modulosDAM);
 
-        
         // Recorremos la lista de modulos de DAW
-        
-        
+        while (iterDAW.hasNext()) {
+            String elemento = iterDAW.next();
+            if (!elemento.contains("i")) {
+                elemento = "**" + elemento + "**";
+                modulosMatriculadosSet.add(elemento);
+                modulosmatriculadosList.add(elemento);
+            }
+        }
+
         // Ordenación de la lista por nombre del módulo (alfabético) y la mostramos por pantalla
-        System.out.println();  
-        System.out.printf ("Ordenación de la lista de módulos de DAM por nombre (alfabético):\n");
+        System.out.println();
+        System.out.printf("Ordenación de la lista de módulos de DAM por nombre (alfabético):\n");
+        Collections.sort(modulosDAM, new ComparadorModuloPorNombre());
+        Utilidades.mostrarModulos(modulosDAM);
 
-               
         // Ordenación de la lista por código del módulo y la mostramos por pantalla
-        System.out.println();  
-        System.out.printf ("Ordenación de la lista de módulos de DAM por código:\n");
+        System.out.println();
+        System.out.printf("Ordenación de la lista de módulos de DAM por código:\n");
+        Collections.sort(modulosDAM, new ComparadorModuloPorCodigo());
+        Utilidades.mostrarModulos(modulosDAM);
 
-        
         //----------------------------------------------
         //            Salida de resultados
         //----------------------------------------------
-        
-        System.out.println();     
+        System.out.println();
         System.out.printf("Contenido final de la lista de módulos de DAW:  \n");
+        Utilidades.mostrarModulos(modulosDAW);
 
-        
-        System.out.println();   
+        System.out.println();
         System.out.printf("Contenido final de la lista de módulos matriculados (DAW): \n");
-    
-                
-        System.out.println();   
+        Utilidades.mostrarModulos(modulosmatriculadosList);
+
+        System.out.println();
         System.out.printf("Contenido final del conjunto de módulos matriculados (DAW): \n");
-   
+        Utilidades.mostrarModulos(modulosMatriculadosSet);
 
     }
+
 }
 
 /**
- * Clase que permite comparar dos módulos usando como criterio
- * de comparación su nombre. Se trata de una comparación alfabética.
- * @author 
+ * Clase que permite comparar dos módulos usando como criterio de comparación su
+ * nombre. Se trata de una comparación alfabética.
+ *
+ * @author José Bueno Cruz
  */
+class ComparadorModuloPorNombre implements Comparator<String> {
 
+    @Override
+    public int compare(String modulo1, String modulo2) {
+
+        String nombre1 = modulo1.split("-")[1];
+        String nombre2 = modulo2.split("-")[1];
+
+        return nombre1.compareTo(nombre2);
+
+    }
+
+}
 
 /**
- * Clase que permite comparar dos módulos usando como criterio
- * de comparación su código.
- * @author 
+ * Clase que permite comparar dos módulos usando como criterio de comparación su
+ * código.
+ *
+ * @author José bueno Cruz
  */
+class ComparadorModuloPorCodigo implements Comparator<String> {
+
+    @Override
+    public int compare(String modulo1, String modulo2) {
+        
+        String codigo1 = modulo1.split("-")[0];
+        String codigo2 = modulo1.split("-")[0];
+
+        return codigo1.compareTo(codigo2);
+    }
+
+}
+
