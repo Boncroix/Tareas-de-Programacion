a3f95d21b4df304f84849f13ff3e2c3886522d12
Boncroix <boncroix@gmail.com>
2025-04-02 21:07:44 +0200

    Ejercicio1 terminado

--- Bueno_Cruz_Jose_PROG_Tarea08/src/ejercicio1/Ejercicio1.java
+++ Bueno_Cruz_Jose_PROG_Tarea08/src/ejercicio1/Ejercicio1.java
@@ -8,14 +8,14 @@ import java.util.*;
 /**
  * Ejercicio 1: Lectura/escritura de una biblioteca de libros en ficheros de texto.
  *
- * @author apellidos y nombre del alumno
+ * @author Bueno Cruz José
  */
 public class Ejercicio1 {
 
     /**
-     * MÃ©todo principal.
+     * Método principal.
      *
-     * @param args argumentos que recibe el mÃ©todo
+     * @param args argumentos que recibe el método
      */
     public static void main(String args[]) {
 
@@ -26,43 +26,42 @@ public class Ejercicio1 {
         // Variables de entrada
         // Variables de salida
         // Variables auxiliares
+        String rutaFichero;
         Biblioteca biblioteca = new Biblioteca();
+        String[] atributos;
 
         //----------------------------------------------
         //       Entrada de datos + Procesamiento
         //----------------------------------------------
-        // Abrimos archivo de contactos ListadoLibros.txt
+        // Leemos el archivo "ListadoLibros.txt" que contiene la información de los libros
         System.out.println("Abriendo archivo de libros...");
+        // Definimos la ruta del archivo de entrada
+        rutaFichero = System.getProperty("user.dir") + "/recursos/ListadoLibros.txt";
         
-        
-        String rutaFichero = System.getProperty("user.dir") + "/recursos/ListadoLibros.txt";
-        
-        try (BufferedReader br = new BufferedReader(new FileReader(rutaFichero))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaFichero))){
             
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                String[] atributos = linea.split(";");
+            String linea; 
+            // Leemos cada línea del archivo
+            while ((linea = br.readLine()) != null) {  
+                // Dividimos la línea usando el delimitador ";"
+                atributos = linea.split(";");
                 String titulo = atributos[0];
                 String autor = atributos[1];
                 String fechaCreacionStr = atributos[2];
+                // Parseamos la fecha de creación
                 LocalDate fechaCreacion = LocalDate.parse(fechaCreacionStr, DateTimeFormatter.ISO_LOCAL_DATE);
                 String genero = atributos[3];
-                String[] capitulosArray = atributos[4].split(",");
-                
-                List<String> capitulos = new ArrayList<>();
-                capitulos.addAll(Arrays.asList(capitulosArray));
-                
+                // Creamos una lista con los capítulos, separando por comas
+                List<String> capitulos = Arrays.asList(atributos[4].split(","));
+                // Creamos un objeto 'Libro' y lo añadimos a la 'biblioteca'
                 Libro libro = new Libro(titulo, autor, fechaCreacion, capitulos, genero);
-                
                 biblioteca.add(libro);
-                
             }
-
-        } catch (IOException e) {
+            
+        } catch (Exception e) {
+            // En caso de error, imprimimos el mensaje correspondiente
             System.err.println("Error al leer el fichero: " + e.getMessage());
         }
-        
-        
 
         System.out.println("Cerrando archivo de libros...");
 
@@ -72,17 +71,44 @@ public class Ejercicio1 {
         //              Salida de resultados 
         //----------------------------------------------
         
-            // Abrimos el archivo de la Biblioteca de libros Biblioteca.txt
-            System.out.println("Abriendo archivo de la biblioteca...");
+        // Ahora generamos el archivo "biblioteca.txt" con la información de la biblioteca
+        System.out.println("Abriendo archivo de la biblioteca...");
+        // Definimos la ruta del archivo de salida
+        rutaFichero = System.getProperty("user.dir") + "/recursos/biblioteca.txt";
+        // Obtenemos la representación en cadena de la biblioteca
+        String toString = biblioteca.toString();
+        
+        try (OutputStreamWriter fichero = new OutputStreamWriter(new FileOutputStream(rutaFichero), "UTF-8")) {
+            // Escribimos un encabezado en el archivo
+            fichero.write("**********************************************************************************************\n"); 
+            fichero.write("LIBRO DE LIBROS \n"); 
+            fichero.write("**********************************************************************************************\n"); 
+            // Dividimos el string resultante de la biblioteca en líneas
+            String[] lineas = toString.split("\n");
+            // Recorremos cada línea para escribir la información de cada libro
+            for (String linea : lineas) {
+                // Dividimos la línea usando el delimitador ","
+                atributos = linea.split(";");
+                // Escribimos los detalles de cada libro en el archivo
+                fichero.write("TITULO DEL LIBRO:" + atributos[0] + "\n"); 
+                fichero.write("AUTOR:" + atributos[1] + "\n"); 
+                fichero.write("FECHA DE CREACIÓN:" + atributos[2] + "\n"); 
+                fichero.write("GENERO:" + atributos[3] + "\n"); 
+                fichero.write("CAPITULOS:" + atributos[4] + "\n");
+                fichero.write("**********************************************************************************************\n");
+            }
 
-           
+        } catch (IOException e) {
+            // En caso de error, imprimimos el mensaje correspondiente
+            System.err.println("Error al crear el fichero: " + e.getMessage());
+        }
 
-            System.out.println("Cerrando archivo de la biblioteca...");
+        System.out.println("Cerrando archivo de la biblioteca...");
 
-            System.out.println();
-            System.out.println("Archivos cerrados y procesamiento finalizado");
-            System.out.println("---------");
-            System.out.println();
-            System.out.println("Fin del programa.");
+        System.out.println();
+        System.out.println("Archivos cerrados y procesamiento finalizado");
+        System.out.println("---------");
+        System.out.println();
+        System.out.println("Fin del programa.");
     }
 }
